bf2f5adef1e6ce4a991a6e5efc77cfad557d6d21
Sudhir Kumar <sudhir9122kumar@gmail.com>
2025-04-06 23:20:21 +0530

    Implemented logout and removed some functionalities

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     const isAdmin = document.getElementById('isAdmin');
-    const logoutButton = document.getElementById('logout-button');
+    const logoutButton = document.getElementById('logout_btn');
     const inventory = document.getElementById('inventory');
     const sales = document.getElementById('sales');
     const transactions = document.getElementById('transactions');
-    const summary = document.getElementById('summary');
-    const reports = document.getElementById('reports');
     const users = document.getElementById('users');
+    const transactions_div = document.getElementById('transactions_div');
+    const users_div = document.getElementById('users_div');
 
     const name = localStorage.getItem('name');
     const is_admin = localStorage.getItem('isAdmin');
@@ -15,8 +15,8 @@ document.addEventListener("DOMContentLoaded", function() {
         isAdmin.innerHTML = `Admin`;
         isAdmin.style.background = 'red';
         isAdmin.style.color = 'white';
-        reports.classList.remove('hidden');
-        users.classList.remove('hidden');
+        users_div.classList.remove('hidden');
+        transactions_div.classList.remove('hidden');
 
     } else {
         isAdmin.innerHTML = `User`;
@@ -24,5 +24,20 @@ document.addEventListener("DOMContentLoaded", function() {
 
     inventory.addEventListener('click', function() {
         window.location.href = 'inventory.html';
+    });
+    logoutButton.addEventListener('click', function() {
+        localStorage.removeItem('name');
+        localStorage.removeItem('isAdmin');
+        window.location.href = 'index.html';
+    }
+    );
+    sales.addEventListener('click', function() {
+        window.location.href = 'sales.html';
+    });
+    transactions.addEventListener('click', function() {
+        window.location.href = 'transactions.html';
+    });
+    users.addEventListener('click', function() {
+        window.location.href = 'users.html';
     });
 })
